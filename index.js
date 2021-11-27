e84b94d0b3d854ff6b58703484c71d065e539e2b
mobius <mobius.daemon@gmail.com>
2021-11-26 22:06:47 -0500

    make it better

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function tipo(text = '') {
   return count_1 == count_2 ? 'balanceado' : 'desbalanceado';
 }
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
