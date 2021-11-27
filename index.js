d55e2e81e5096d5abb065cd72151e5e29190c178
mobius <mobius.daemon@gmail.com>
2021-11-26 21:59:26 -0500

    make it better

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const nunjucks = require('nunjucks');
+
+const _templates = path.join(__dirname, './views');
+
+const nunjucksEnv = nunjucks.configure(_templates, {
+  autoescape: true,
+  express: app,
+  noCache: true,
+});
+
+app.set('view engine', 'njk');
+
+app.get('/', function (req, res) {
+  const result = tipo(req.query.word);
+  res.render('index', { result, word: req.query.word });
+});
+
+function tipo(text = '') {
+  const reg = /\({1}[a-zA-Z0-9\:]\){1}/gi;
+
+  text = text.replace(reg, '').replace(/\:\)/gi, '').replace(/\:\)/gi, '');
+
+  const count_1 = (text.match(/\)/g) || []).length;
+  const count_2 = (text.match(/\(/g) || []).length;
+  console.log(count_1, count_2);
+  console.log(text);
+  return count_1 == count_2 ? 'balanceado' : 'desbalanceado';
+}
+
+const port = process.env.port || 5000;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
